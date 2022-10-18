17c3c3bdce71684af84a7b12eb98d06c50bc5461
Rajdeep1060 <rajdeep1060@chitkara.edu.in>
2022-10-18 11:31:57 +0530

    first commit

--- src/containers/AdvancedSearchContainer.js
+++ src/containers/AdvancedSearchContainer.js
@@ -81,7 +81,7 @@ const AdvancedSearchContainer = () => {
           <Form.FormGroup>
             <Form.Span>
               {" "}
-              Price range: Ksh {priceFormat(+priceRange)} to Ksh{" "}
+              Price range: Rs. {priceFormat(+priceRange)} to Rs.{" "}
               {priceFormat(maxPrice)}
             </Form.Span>
             <Form.RangeInput
